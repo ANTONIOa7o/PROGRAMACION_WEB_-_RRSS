968567f07f01496d20f33d0e8236bf090aa2adec
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-12-12 13:44:38 +0100

    Add files via upload

---
+++ [C]-EJECUCION/2-INICIAL_JS/ejercicio4-funciones/js/script6.js
@@ -0,0 +1,9 @@
+//1º   Declaracion de variables locales
+
+function cta() {
+    var usuario = document.getElementById('user').value;
+    /* Importante:  Reduccion de recogida de variable, es decir, script
+    va adaptandose y muta a menores caracteres.  */
+    var pasword = pw.value;
+    
+}
