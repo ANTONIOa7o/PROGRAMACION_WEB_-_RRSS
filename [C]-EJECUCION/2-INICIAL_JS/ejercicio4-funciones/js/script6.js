fc3139de0940cad5e09bc366ab8f2f660ea28aa0
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-12-13 13:35:37 +0100

    Add files via upload

--- [C]-EJECUCION/2-INICIAL_JS/ejercicio4-funciones/js/script6.js
+++ [C]-EJECUCION/2-INICIAL_JS/ejercicio4-funciones/js/script6.js
@@ -5,5 +5,16 @@ function cta() {
     /* Importante:  Reduccion de recogida de variable, es decir, script
     va adaptandose y muta a menores caracteres.  */
     var pasword = pw.value;
+    document.getElementById('resultado').innerHTML=
+    'Acceso permitido: ' + usuario + ' Con clave: ' + pasword;
+    
+}
+function borrar() {
+    document.getElementById('resultado').innerHTML=''
+}
+
+function redireccionar() {
+   /*  location.href='https://www.bet365.es/' */
+   location.href='5-variablesFuncionales.html'
     
 }
