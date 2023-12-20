df3e1d045c0e6dc0dc54e21e969e4b346551889c
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-12-20 13:42:45 +0100

    Add files via upload

--- [C]-EJECUCION/2-INICIAL_JS/ejercicio5-EstructurasControl/B]-SWITCH_CASE/js/script4.js
+++ [C]-EJECUCION/2-INICIAL_JS/ejercicio5-EstructurasControl/B]-SWITCH_CASE/js/script4.js
@@ -6,18 +6,24 @@ function inicioReloj() {
     var horas = hoy.getHours();
     var minutos = hoy.getMinutes();
     var segundos = hoy.getSeconds();
+    
+    /* Uso de la maquina perfecta o funcion perfecta para generar las  decenas ↓ */
+    segundos = decenas(segundos);
+    minutos= decenas(minutos);
+    horas = decenas(horas);
+
     //3º Variables secundarias del Calendario 
-  var diaSemana = hoy.toLocaleDateString('es-eu',{weekday:'long'});
+    var diaSemana = hoy.toLocaleDateString('es-eu', { weekday: 'long' });
     var dia = hoy.getDate();
     var mes = hoy.getMonth();
     var año = hoy.getFullYear();
     //Mostramos Reloj
     document.getElementById('reloj').innerHTML =
-     '' + horas + ':' + minutos + ':' + segundos;
+        '' + horas + ':' + minutos + ':' + segundos;
 
     //Gestionando la variable mes del Calendario
     switch (mes) {
-       
+
         case 0:
             mes = 'Enero';
             break;
@@ -51,16 +57,39 @@ function inicioReloj() {
         case 10:
             mes = 'Noviembre';
             break;
-            case 11:
-                mes = 'Diciembre';
-                break;
+        case 11:
+            mes = 'Diciembre';
+            break;
 
         default:
-            mes= 'Error 404# falta de conexion'
+            mes = 'Error 404# falta de conexion'
             break;
     }
-//Mostramos el Calendario
+    //Mostramos el Calendario
+    document.getElementById('calendario').innerHTML = diaSemana +
+        ' ' + dia + ' ' + mes + ' ' + año;
+       
+       
+        
+  /* Ejecuciones de funcion a partir de un parametro de tiempo definido
+  Dos formas de ejecucion:
+    a) Ejecucion Clasica:
+        setTimeout(funcion(){funcionAllamar(),funcionAllarmar2},temporizamos en milisegundos);
 
-document.getElementById('calendario').innerHTML = diaSemana + 
-' ' + dia + ' ' + mes + ' ' + año;
+    b) Ejecucion mutada:
+        var t = setTimeout(()=>{funcionAllamar(),funcionAllarmar2},timeout); 
+  
+  */      
+setTimeout(function(){inicioReloj()},1000);
+}
+/* Ejecucion de una funcion perfecta, será la ejecucion de referencia por donde quiera
+hacer pasar una variable
+Aparecerá  un concepto nuevo, necesario de definir como es el  return
+*/
+function decenas(x) {
+
+    if (x<10) {
+       x = '0'+ x 
+    }
+    return (x);
 }
